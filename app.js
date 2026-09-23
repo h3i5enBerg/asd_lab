a06882b474eedc85dfa199895af318c22214abf6
niraj <nirajssatam@gmail.com>
2026-09-24 00:03:01 +0530

    patch

--- app.js
+++ app.js
@@ -40,6 +40,31 @@ app.get('/assignments', async (req, res) => {
     }
 });
 
+app.patch('/assignments/:id', async(req, res) => {
+    try{
+        const { id } = req.params;
+        const result = await pool.query(
+            `UPDATE assignments
+            SET submitted = true
+            WHERE id = $1
+            RETURNING *;`,
+            [id]
+        );
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                message: 'Assignments Not Found'
+            });
+        };
+        res.status(200).json(result.rows[0]);
+    }catch(err){
+    console.log(err.message);
+    res.status(502).json({
+        errorMessage: 'Server Band Ha Sir'
+    });
+    }
+});
+
+
 app.listen(PORT, () => {
     console.log('Welcome to the server Null Vector');
 })
